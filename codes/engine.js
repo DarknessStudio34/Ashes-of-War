9a4c5eb740272199dc91e0612d67c4e1ce878fbe
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-01-20 11:40:46 -0300

    Create engine.js

---
+++ codes/engine.js
@@ -0,0 +1,21 @@
+var screenX = window.screen.width, screenY = window.screen.height
+function setup (){
+  createCanvas(screenX, screenY);
+}
+function draw (){
+  screenConfig();
+  cenarios();
+  frames();
+}
+function preload (){
+  
+}
+function mouseClicked (){
+  
+}
+function frames (){
+  
+}
+function screenConfig (){
+  
+}
