a692e0fec730152a30bb797156bf16fa8412dd65
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-02-19 12:43:24 -0300

    Update cenas.js

--- codes/cenas.js
+++ codes/cenas.js
@@ -1,10 +1,25 @@
 var c = 0
 function cenarios (){
-   if (c == 0){background(200)}
+   if (propor == 20.9){
+     if(c == 0){
+       background (loading.20_9)
+       tld20_1();
+     }
+   }
+   else if (propor == 16.9){
+     if(c == 0){
+       background (loading.16_9)
+       tld16_1();
+     }
+   }
 }
 function tld20_1 (){
-  
+  setTimeOut(
+    c = 1;
+  )
 } 
 function tld16_1 (){
-  
+  setTimeOut(
+    c = 1;
+  )
 }  
