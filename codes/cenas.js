ecf3cf318c93b8fd6bd7f41b56c749461253a7e2
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-03-09 16:35:16 -0300

    Update cenas.js

--- codes/cenas.js
+++ codes/cenas.js
@@ -8,10 +8,10 @@ function cenarios (){
          background(graphics.bg)
          image(graphics.btJ, screenX * 0.80, screenY * 0.25, screenY * 0.35, screenY * 0.35);
          image(graphics.btO, screenX * 0.80, screenY * 0.55, screenY * 0.35, screenY * 0.35);
-         if (mouseX > screenX * 0.825 && mouse < screenX * 0.95 && mouseY > screenY * 0.29 && mouseY < screenY * 0.463){
+         if (mouseX > screenX * 0.825 && mouseX < screenX * 0.95 && mouseY > screenY * 0.29 && mouseY < screenY * 0.463){
            c = 1.1
          }
-         if (mouseX > screenX * 0.825 && mouse < screenX * 0.95 && mouseY > screenY * 0.61 && mouseY < screenY * 0.73){
+         if (mouseX > screenX * 0.825 && mouseX < screenX * 0.95 && mouseY > screenY * 0.61 && mouseY < screenY * 0.73){
            c = 1.2
          }
        } else if (c == 1.1){
