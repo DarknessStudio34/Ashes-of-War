d2c0382b0a855fee19f123dd7ae8e3f9b3d2e6d1
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-03-18 11:11:33 -0300

    Update image-loader.js

--- codes/image-loader.js
+++ codes/image-loader.js
@@ -3,6 +3,7 @@ o p5 roda o jogo de forma linear, ou seja, em apenas um núcleo do processador.
 Isso resulta em diversos problemas de desempenho, uma solução pra isso é usar o Worker.
 O Worker basicamente é um gerenciador de thread, então cálculos pesados, imagens e etc podem ser executados em segundo plano
  
+*/
 self.onmessage = function(event) {
   var imageList = event.data;
 
