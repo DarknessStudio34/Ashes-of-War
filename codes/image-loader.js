0e6eee6fcc721c97cdde02734cdce64b28c4893c
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-03-18 11:03:07 -0300

    Update image-loader.js

--- codes/image-loader.js
+++ codes/image-loader.js
@@ -1,1 +1,16 @@
 
+self.onmessage = function(event) {
+  var imageList = event.data;
+
+  // Carregar todas as imagens em paralelo usando Promise.all()
+  Promise.all(imageList.map(loadImageAsBitmap)).then(function(values) {
+    // Enviar as imagens processadas de volta para o arquivo principal do p5.js
+    self.postMessage(values);
+  });
+}
+
+function loadImageAsBitmap(imageUrl) {
+  return fetch(imageUrl)
+    .then(response => response.blob())
+    .then(blob => createImageBitmap(blob));
+}
