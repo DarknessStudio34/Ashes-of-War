cd75a6e9c2e3745c226a0695f7772cae2099f823
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-03-18 21:18:31 -0300

    Update image-loader.js

--- codes/image-loader.js
+++ codes/image-loader.js
@@ -1,21 +1,30 @@
-/*image-loader.js: 
-o p5 roda o jogo de forma linear, ou seja, em apenas um núcleo do processador.
-Isso resulta em diversos problemas de desempenho, uma solução pra isso é usar o Worker.
-O Worker basicamente é um gerenciador de thread, então cálculos pesados, imagens e etc podem ser executados em segundo plano
-E não comprometendo o desempenho do jogo
-*/
-self.onmessage = function(event) {
-  var imageList = event.data;
+onmessage = function(e) {
+  if (e.data.type === 'start_tld20_1') {
+    if (e.data.ld === 1) {
+      importScripts('p5.js');
+      var graphics = {};
+      var c = 0;
+      
+      function tld20_1() {
+        var img0 = loadImage('assets/20_9/int/mp_bg.png');
+        var img1 = loadImage('assets/int_un/Mike-fl-scene1.png');
+        var img2 = loadImage('assets/int_un/Allan-fl-scene1.png');
+        var img3 = loadImage('assets/int_un/Pai-fl-scene1.png');
+        var img4 = loadImage('assets/int_un/bt_jogar.png');
+        var img5 = loadImage('assets/int_un/bt_options.png');
 
-  // Carregar todas as imagens em paralelo usando Promise.all()
-  Promise.all(imageList.map(loadImageAsBitmap)).then(function(values) {
-    // Enviar as imagens processadas de volta para o arquivo principal do p5.js
-    self.postMessage(values);
-  });
-}
-
-function loadImageAsBitmap(imageUrl) {
-  return fetch(imageUrl)
-    .then(response => response.blob())
-    .then(blob => createImageBitmap(blob));
-}
+        Promise.all([img0, img1, img2, img3, img4, img5]).then(function(values) {
+          graphics.bg = values[0];
+          graphics.flM = values[1];
+          graphics.flA = values[2];
+          graphics.flP = values[3];
+          graphics.btJ = values[4];
+          graphics.btO = values[5];
+          c = 1;
+          postMessage({type: 'tld20_1_done', graphics: graphics});
+        });
+      }
+      tld20_1();
+    }
+  }
+};
