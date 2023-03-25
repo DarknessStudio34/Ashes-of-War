6893622c2b16ed8a9252dae36f9d6a6cef42ce99
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-03-25 20:32:03 -0300

    Update cenas.js

--- codes/cenas.js
+++ codes/cenas.js
@@ -25,7 +25,8 @@ function cenarios (){
      worker.postMessage('loadImages');
      } else {
      // fallback para navegadores que não suportam worker
-     tld20_1();
+         tld20_1();
+         console.log('incompatível com Worker');
        }
      } else if (c == 1){
          background(graphics.bg)
@@ -51,4 +52,44 @@ function cenarios (){
          background(graphics.bg)
        }
    }
+}
+
+
+function tld20_1() {
+  var img0 = loadImage('assets/20_9/int/mp_bg.png');
+  var img1 = loadImage('assets/int_un/Mike-fl-scene1.png');
+  var img2 = loadImage('assets/int_un/Allan-fl-scene1.png');
+  var img3 = loadImage('assets/int_un/Pai-fl-scene1.png');
+  var img4 = loadImage('assets/int_un/bt_jogar.png');
+  var img5 = loadImage('assets/int_un/bt_options.png')
+
+  Promise.all([img0, img1, img2, img3, img4, img5]).then(function(values) {
+    graphics.bg = values[0];
+    graphics.flM = values[1];
+    graphics.flA = values[2];
+    graphics.flP = values[3];
+    graphics.btJ = values[4];
+    graphics.btO = values[5];
+    c = 1;
+  });
+}
+
+
+function tld16_1() {
+  var img0 = loadImage('assets/16_9/int/mp_bg.png');
+  var img1 = loadImage('assets/int_un/Mike-fl-scene1.png');
+  var img2 = loadImage('assets/int_un/Allan-fl-scene1.png');
+  var img3 = loadImage('assets/int_un/Pai-fl-scene1.png');
+  var img4 = loadImage('assets/int_un/bt_jogar.png');
+  var img5 = loadImage('assets/int_un/bt_options.png')
+
+  Promise.all([img0, img1, img2, img3, img4, img5]).then(function(values) {
+    graphics.bg = values[0];
+    graphics.flM = values[1];
+    graphics.flA = values[2];
+    graphics.flP = values[3];
+    graphics.btJ = values[4];
+    graphics.btO = values[5];
+    c = 1;
+  });
 }
