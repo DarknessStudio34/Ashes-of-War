c1508a7659a720bcaf9b0918f53b4e166f2efa13
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-03-25 20:16:20 -0300

    Update cenas.js

--- codes/cenas.js
+++ codes/cenas.js
@@ -1,75 +1,34 @@
-const worker = new Worker("codes/worker.js");
-
+var graphics = {};
 var c = 0;
 
-function cenarios (){
-   if (propor == 20.9){
-     if(c == 0){
-       background (loading.tld20_9);
-       worker.postMessage("t1")
-       tld20_1();
-     } else if (c == 1){
-         background(graphics.bg);
-         image(graphics.btJ, screenX * 0.80, screenY * 0.25, screenY * 0.35, screenY * 0.35);
-         image(graphics.btO, screenX * 0.80, screenY * 0.55, screenY * 0.35, screenY * 0.35);
-         if (mouseX > screenX * 0.825 && mouseX < screenX * 0.95 && mouseY > screenY * 0.29 && mouseY < screenY * 0.463){
-           c = 1.1;
-         }
-         if (mouseX > screenX * 0.825 && mouseX < screenX * 0.95 && mouseY > screenY * 0.61 && mouseY < screenY * 0.73){
-           c = 1.2;
-         }
-     } else if (c == 1.1){
-         background(graphics.bg);
-     } else if (c == 1.2){
-         background(graphics.bg);
-     }
-   } else if (propor == 16.9){
-     if(c == 0){
-       background (loading.tld16_9);
-       tld16_1();
-     } else if (c == 1){
-         background(graphics.bg);
-     }
-   }
+function cenarios() {
+  if (propor == 20.9) {
+    if (c == 0){}
+  }
+  else if (propor == 16.9){
+    if (c == 0){}
+  }
 }
 
-var graphics = {};
-
-function tld20_1() {
-  var img0 = loadImage('assets/20_9/int/mp_bg.png');
-  var img1 = loadImage('assets/int_un/Mike-fl-scene1.png');
-  var img2 = loadImage('assets/int_un/Allan-fl-scene1.png');
-  var img3 = loadImage('assets/int_un/Pai-fl-scene1.png');
-  var img4 = loadImage('assets/int_un/bt_jogar.png');
-  var img5 = loadImage('assets/int_un/bt_options.png')
-
-  Promise.all([img0, img1, img2, img3, img4, img5]).then(function(values) {
-    graphics.bg = values[0];
-    graphics.flM = values[1];
-    graphics.flA = values[2];
-    graphics.flP = values[3];
-    graphics.btJ = values[4];
-    graphics.btO = values[5];
-    c = 1;
-  });
-}
-
-
-function tld16_1() {
-  var img0 = loadImage('assets/16_9/int/mp_bg.png');
-  var img1 = loadImage('assets/int_un/Mike-fl-scene1.png');
-  var img2 = loadImage('assets/int_un/Allan-fl-scene1.png');
-  var img3 = loadImage('assets/int_un/Pai-fl-scene1.png');
-  var img4 = loadImage('assets/int_un/bt_jogar.png');
-  var img5 = loadImage('assets/int_un/bt_options.png')
-
-  Promise.all([img0, img1, img2, img3, img4, img5]).then(function(values) {
-    graphics.bg = values[0];
-    graphics.flM = values[1];
-    graphics.flA = values[2];
-    graphics.flP = values[3];
-    graphics.btJ = values[4];
-    graphics.btO = values[5];
-    c = 1;
-  });
+if (window.Worker) {
+  var worker = new Worker('codes/worker.js');
+  worker.addEventListener('message', function(e) {
+    if (typeof e.data === 'object' && 'bg' in e.data && 'flM' in e.data && 'flA' in e.data && 'flP' in e.data && 'btJ' in e.data && 'btO' in e.data) {
+      graphics.bg = e.data.bg;
+      graphics.flM = e.data.flM;
+      graphics.flA = e.data.flA;
+      graphics.flP = e.data.flP;
+      graphics.btJ = e.data.btJ;
+      graphics.btO = e.data.btO;
+    }
+    if (typeof e.data === 'number' && e.data === 1) {
+      if (c === 1) {
+        cenarios();
+      }
+    }
+  }, false);
+  worker.postMessage('loadImages');
+} else {
+  // fallback para navegadores que não suportam worker
+  tld20_1();
 }
